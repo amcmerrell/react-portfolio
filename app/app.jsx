f7383f41108bfb32a73088b7d4eb860cb0cc3ef8
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-03-22 15:00:36 -0700

    incorporate sass styling in App and Nav component.

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Nav from './components/Nav';
+import style from './../styles/app.scss';
 
 ReactDOM.render(
   <Nav />,
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 
+
 class Nav extends Component {
   render() {
     return (
-      <h1 style={styles.navContainer}>Navigation</h1>
+      <div className='nav-container'>
+        <h1>Navigation</h1>
+      </div>
     );
   }
 }
 
-const styles = {
-  navContainer: {
-    flex: 1,
-    backgroundColor: 'grey'
-  }
-};
+// const styles = {
+//   navContainer: {
+//     flex: 1,
+//     backgroundColor: 'grey'
+//   }
+// };
 
 export default Nav;
