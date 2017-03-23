5975fcd113b75420a213230d5840e644967f6a62
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-03-22 18:53:24 -0700

    add SubFeature component to house About and Skills sections.

--- app/app.jsx
+++ app/app.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Nav from './components/Nav';
 import Feature from './components/Feature';
+import SubFeature from './components/SubFeature';
 import style from './../styles/app.scss';
 
 ReactDOM.render(
   <div>
     <Nav />
     <Feature />
+    <SubFeature />
   </div>,
   document.getElementById('app')
 );
---
+++ app/components/SubFeature.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubFeature = () => {
+  return (
+    <div className='subfeature-container'>
+      <h1>SubFeature</h1>
+    </div>
+  );
+};
+
+export default SubFeature;
