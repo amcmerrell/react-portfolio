f110792ec726b276f0905dc11ae958d156929f14
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-03-22 18:54:46 -0700

    set Nav component to stick to top of window on scroll.

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-const Nav = () => {
-  return (
-    <div className='nav-container'>
-      <h1>Navigation</h1>
-    </div>
-  );
-};
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLockedToTop: false,
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnMount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    if (window.scrollY > 100) {
+      this.setState({ isLockedToTop: true });
+      console.log('unlocked');
+    } else if (window.scrollY <= 100) {
+      this.setState({ isLockedToTop: false });
+      console.log('locked');
+    }
+  }
+
+  render() {
+    return (
+      <div className='nav-container' style={{ position: this.state.isLockedToTop ? 'fixed' : 'relative' }}>
+        <h1>Navigation</h1>
+      </div>
+    );
+  }
+}
 
 export default Nav;
