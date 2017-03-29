5714427d2caee3294d752ad70415a455efe7c479
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-03-29 11:57:43 -0700

    refactor Nav into a display component and move logic for animation to Main component.

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,11 +5,37 @@ import Feature from './Feature';
 import SubFeature from './SubFeature';
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavLockedToTop: false,
+      isNavHidden: false,
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnMount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    if (window.scrollY > 100) {
+      this.setState({ isNavLockedToTop: true });
+      console.log('nav locked');
+    } else if (window.scrollY < 100) {
+      this.setState({ isNavLockedToTop: false });
+      console.log('nav unlocked');
+    }
+  }
 
   render() {
     return (
       <div>
-        <Nav />
+        <Nav locked={this.state.isNavLockedToTop} />
         <Feature />
         <SubFeature />
       </div>
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,46 +1,16 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import classNames from 'classnames';
 
-class Nav extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLockedToTop: false,
-      isHidden: false,
-    };
-    this.handleScroll = this.handleScroll.bind(this);
-  }
+const Nav = (props) => {
+  const navClass = classNames({
+    'locked-fade-in': props.locked
+  });
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-
-  componentWillUnMount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll() {
-    if (window.scrollY > 100) {
-      this.setState({ isLockedToTop: true });
-      console.log('unlocked');
-    } else if (window.scrollY < 100) {
-      this.setState({ isLockedToTop: false });
-      console.log('locked');
-    }
-  }
-
-  render() {
-    const navClass = classNames({
-      'locked-fade-in': this.state.isLockedToTop
-    });
-
-    return (
-      <nav className={navClass} >
-        <h1>Navigation</h1>
-      </nav>
-    );
-  }
-}
+  return (
+    <nav className={navClass} >
+      <h1>Navigation</h1>
+    </nav>
+  );
+};
 
 export default Nav;
