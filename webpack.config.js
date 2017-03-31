d1bb28883a832ce6d4ab0233fddb4f6325e1462c
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-03-31 10:57:25 -0700

    add image loader and file loader to project.

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,20 @@ module.exports = {
         loaders: ['style', 'css', 'sass'],
         test: /\.scss$/,
       },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 25000,
+        },
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[hash].[ext]',
+        },
+      },
     ]
   }
 };
