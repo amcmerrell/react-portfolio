68cc622df1c59e9c5596234a1375c0f698c03a41
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-04-01 13:40:48 -0700

    add initial mobile nav menu slide-down effect.

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,36 +5,34 @@ import Feature from './Feature';
 import SubFeature from './SubFeature';
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isNavLockedToTop: false,
-    };
-    this.handleScroll = this.handleScroll.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-
-  componentWillUnMount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll() {
-    if (window.scrollY > 100) {
-      this.setState({ isNavLockedToTop: true });
-    } else if (window.scrollY <= 100) {
-      this.setState({ isNavLockedToTop: false });
-    }
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     isNavLockedToTop: false,
+  //   };
+  //   this.handleScroll = this.handleScroll.bind(this);
+  // }
+  //
+  // componentDidMount() {
+  //   window.addEventListener('scroll', this.handleScroll);
+  // }
+  //
+  // componentWillUnMount() {
+  //   window.removeEventListener('scroll', this.handleScroll);
+  // }
+  //
+  // handleScroll() {
+  //   if (window.scrollY > 100) {
+  //     this.setState({ isNavLockedToTop: true });
+  //   } else if (window.scrollY <= 100) {
+  //     this.setState({ isNavLockedToTop: false });
+  //   }
+  // }
 
   render() {
     return (
       <div>
-        <Nav
-          locked={this.state.isNavLockedToTop}
-        />
+        <Nav />
         <Feature />
         <SubFeature />
       </div>
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,7 +3,32 @@ import classNames from 'classnames';
 import SmoothScroll from 'smoothscroll';
 
 class Nav extends Component {
-  onClick(element, e) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavLockedToTop: false,
+      isMobileNavOpen: false
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnMount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    if (window.scrollY > 100) {
+      this.setState({ isNavLockedToTop: true });
+    } else if (window.scrollY <= 100) {
+      this.setState({ isNavLockedToTop: false });
+    }
+  }
+
+  onNavClick(element, e) {
     const home = document.querySelector('.feature-container');
     const subFeature = document.querySelector('.subfeature-container');
 
@@ -16,21 +41,35 @@ class Nav extends Component {
     e.preventDefault();
   }
 
+  onMenuClick(e) {
+    if (this.state.isMobileNavOpen) {
+      this.setState({ isMobileNavOpen: false });
+    } else {
+      this.setState({ isMobileNavOpen: true });
+    }
+
+    e.preventDefault();
+  }
+
   render() {
     const navClass = classNames({
-      'nav-fade-in': this.props.locked,
+      'nav-fade-in': this.state.isNavLockedToTop,
+    });
+
+    const mobileNavClass = classNames({
+      'animate-nav-height': this.state.isMobileNavOpen
     });
     return (
       <nav className={navClass} >
-        <div className='logo'>
+        <div className='logo' onClick={this.onMenuClick.bind(this)}>
           <h3>AM</h3>
         </div>
-        <ul>
+        <ul className={mobileNavClass}>
           <li>
-            <a ref='home' onClick={this.onClick.bind(this, 'home')}>Home</a>
+            <a ref='home' onClick={this.onNavClick.bind(this, 'home')}>Home</a>
           </li>
           <li>
-            <a ref='subFeature' onClick={this.onClick.bind(this, 'subFeature')}>About</a>
+            <a ref='subFeature' onClick={this.onNavClick.bind(this, 'subFeature')}>About</a>
           </li>
         </ul>
 
