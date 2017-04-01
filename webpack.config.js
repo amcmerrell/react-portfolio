9135c2ea8f49844fb760dece1468c7c7bb050e5a
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-03-31 22:57:28 -0700

    update webpack version and syntax used in webpack.config file.

--- webpack.config.js
+++ webpack.config.js
@@ -8,34 +8,21 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    root: __dirname,
-    extensions: ['', '.js', '.jsx']
+    modules: [
+      path.join(__dirname, 'app'),
+      'node_modules'
+    ],
+    extensions: ['.js', '.jsx']
   },
   module: {
-    // rules: [
-    //   {
-    //     test: /\.scss$/,
-    //     use: ExtractTextPlugin.extract({
-    //       loader: ['css', 'postcss', 'sass']
-    //     })
-    //   },
-    //   {
-    //     loader: 'babel-loader',
-    //     query: {
-    //       presets: ['react', 'es2015']
-    //     },
-    //     test: /\.jsx?$/,
-    //     exclude: /(node_modules|bower_components)/
-    //   },
-    //   {
-    //     test: /\.(jpg|png|svg)$/,
-    //     loader: 'file-loader',
-    //     options: {
-    //       name: '[path][name].[hash].[ext]',
-    //     }
-    //   },
-    // ],
-    loaders: [
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      },
       {
         loader: 'babel-loader',
         query: {
@@ -48,31 +35,11 @@ module.exports = {
         test: /\.(jpg|png|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[path][name].[hash].[ext]',
+          name: './img/[name].[hash].[ext]',
         }
       },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract(
-          [
-            { loader: 'css' },
-            { loader: 'postcss' },
-            { loader: 'sass' }
-          ]
-        )
-      }
-      // {
-      //   test: /\.scss$/,
-      //   loaders: ['style', 'css', 'postcss', 'sass'],
-      // },
-    ]
+    ],
   },
-  plugins: [
-    new ExtractTextPlugin('style.css')
-    //if you want to pass in options, you can do so:
-    //new ExtractTextPlugin({
-    //  filename: 'style.css'
-    //})
-  ],
+  plugins: [new ExtractTextPlugin('style.css')],
   devtool: 'cheap-module-eval-source'
 };
