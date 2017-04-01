822578d715f4096d050ab7239c6d0ecf3e4b43c2
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-03-31 22:10:20 -0700

    add extract-text-webpack-plugin and implement in order to extract css into separate file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './app/app.jsx',
@@ -8,11 +9,32 @@ module.exports = {
   },
   resolve: {
     root: __dirname,
-    alias: {
-    },
     extensions: ['', '.js', '.jsx']
   },
   module: {
+    // rules: [
+    //   {
+    //     test: /\.scss$/,
+    //     use: ExtractTextPlugin.extract({
+    //       loader: ['css', 'postcss', 'sass']
+    //     })
+    //   },
+    //   {
+    //     loader: 'babel-loader',
+    //     query: {
+    //       presets: ['react', 'es2015']
+    //     },
+    //     test: /\.jsx?$/,
+    //     exclude: /(node_modules|bower_components)/
+    //   },
+    //   {
+    //     test: /\.(jpg|png|svg)$/,
+    //     loader: 'file-loader',
+    //     options: {
+    //       name: '[path][name].[hash].[ext]',
+    //     }
+    //   },
+    // ],
     loaders: [
       {
         loader: 'babel-loader',
@@ -30,10 +52,27 @@ module.exports = {
         }
       },
       {
-        loaders: ['style', 'css', 'postcss', 'sass'],
         test: /\.scss$/,
-      },
+        loader: ExtractTextPlugin.extract(
+          [
+            { loader: 'css' },
+            { loader: 'postcss' },
+            { loader: 'sass' }
+          ]
+        )
+      }
+      // {
+      //   test: /\.scss$/,
+      //   loaders: ['style', 'css', 'postcss', 'sass'],
+      // },
     ]
   },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+    //if you want to pass in options, you can do so:
+    //new ExtractTextPlugin({
+    //  filename: 'style.css'
+    //})
+  ],
   devtool: 'cheap-module-eval-source'
 };
