2ad4cbc8f3e4468485bb31dd1797f119c93e89a2
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-04-05 15:17:40 -0700

    include react-css-transition-group in project and implement in feature container.

--- app/components/Feature.jsx
+++ app/components/Feature.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import andrew from './../img/portfolio-andrew.png';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 const Feature = () => {
   const featureClasses = classNames({
@@ -10,11 +11,17 @@ const Feature = () => {
 
   return (
     <div className={featureClasses}>
-      <div className='feature'>
-        <img alt='Andrew Merrell' src={andrew} />
-        <h3>Andrew Merrell</h3>
-        <h4>Jr. Software Developer</h4>
-      </div>
+      <ReactCSSTransitionGroup
+        transitionName='feature-transition'
+        transitionAppear={true}
+        transitionAppearTimeout={1500}
+      >
+        <div className='feature'>
+          <img alt='Andrew Merrell' src={andrew} />
+          <h3>Andrew Merrell</h3>
+          <h4>Jr. Software Developer</h4>
+        </div>
+      </ReactCSSTransitionGroup>
     </div>
   );
 };
