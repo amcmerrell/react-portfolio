0b539790a6c2562bf2c58c37198d4ece5e620f55
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-04-07 19:28:54 -0700

    move Main and SubFeature to new containers folder.

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import style from './styles/app.scss';
-import Main from './components/Main';
+import Main from './containers/Main';
 import Nav from './components/Nav';
 
 ReactDOM.render(
