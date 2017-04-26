fb0d905b06d960a39c4d8bd1edd9b4190b1c6884
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-04-26 16:20:55 -0700

    add devicons font icons to project and test in SubFeature.

--- app/containers/SubFeature.jsx
+++ app/containers/SubFeature.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import classNames from 'classnames';
 
 const SubFeature = () => {
+  const androidIconClass = classNames({
+    'devicon': true,
+    'devicon-android-plain': true,
+    'colored': true
+  });
+
   return (
     <div className='subfeature-container'>
       <h1>SubFeature</h1>
+      <i className={androidIconClass}></i>
     </div>
   );
 };
