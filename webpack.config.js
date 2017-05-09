383281134830082d6150c1451fee03bbc1962e7f
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-05-09 15:33:34 -0700

    add plugin to minify css.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const webpack = require('webpack');
 const CompressionPlugin = require('compression-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: './app/app.jsx',
@@ -118,6 +119,7 @@ module.exports = {
     }),
     new FaviconsWebpackPlugin('./app/img/am-logo.png'),
     new ExtractTextPlugin('style.css'),
+    new OptimizeCssAssetsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
