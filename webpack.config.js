8237847cd22fd565fafc6e282157de59b47626a6
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-09-14 22:42:15 -0700

    comment out compression plugin in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -135,13 +135,13 @@ module.exports = {
     }),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
-    new CompressionPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8
-    })
+    // new CompressionPlugin({
+    //   asset: '[path].gz[query]',
+    //   algorithm: 'gzip',
+    //   test: /\.js$|\.css$|\.html$/,
+    //   threshold: 10240,
+    //   minRatio: 0.8
+    // })
   ],
   devtool: 'cheap-module-eval-source'
 };
